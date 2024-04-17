319cd727ce3e975dcc34431fe33787552ab27ddf
Italo Menezes <italomlp@gmail.com>
2024-04-17 13:24:07 -0300

    feat: update descriptions for platform props

--- components/territorio/platform/types.ts
+++ components/territorio/platform/types.ts
@@ -1,26 +1,38 @@
-import { ImageWidget } from "apps/admin/widgets.ts";
+import { ImageWidget } from "apps/admin/widgets.ts"
 
 export interface SectionTitle {
   /** @title Main title */
-  main: string;
+  main: string
   /** @title Subtitle */
-  sub: string;
+  sub: string
 }
 
 export interface Section {
-  title: string;
-  content: string;
+  /** @title Title */
+  title: string
+  /** @title Content */
+  content: string
 }
 
 export interface Content {
-  section1: Section;
-  section2: Section;
-  section3: Section;
-  section4: Section;
-  section5: Section;
+  /** @title Section 1 */
+  /** @description top left */
+  section1: Section
+  /** @title Section 2 */
+  /** @description top right */
+  section2: Section
+  /** @title Section 3 */
+  /** @description middle right */
+  section3: Section
+  /** @title Section 4 */
+  /** @description bottom left */
+  section4: Section
+  /** @title Section 5 */
+  /** @description bottom right */
+  section5: Section
 }
 
 export interface ContentProps {
-  content: Content;
-  image: ImageWidget;
+  content: Content
+  image: ImageWidget
 }
