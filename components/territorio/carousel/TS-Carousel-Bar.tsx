3425af79193cbaec1689db6c39ad81a6064e14b8
Guilherme <guilherme.brunner@gmail.com>
2024-04-19 13:51:51 -0300

    hotfix(frontend): fixes CarouselBar selected color

--- components/territorio/carousel/TS-Carousel-Bar.tsx
+++ components/territorio/carousel/TS-Carousel-Bar.tsx
@@ -1,14 +1,14 @@
-import { clx } from "../../../sdk/clx.ts";
+import { clx } from '../../../sdk/clx.ts';
 import {
   ComplementaryColors,
   ThemeColors,
-} from "../../../sections/Theme/Theme.tsx";
+} from '../../../sections/Theme/Theme.tsx';
 
 type Color =
   | keyof ThemeColors
   | keyof ComplementaryColors
-  | "accent"
-  | "accent-content";
+  | 'accent'
+  | 'accent-content';
 
 export interface Props {
   items: unknown[];
@@ -21,12 +21,13 @@ const TsCarouselBar = ({
   items,
   currentId,
   onItemClick,
-  activeColor = "accent-content",
+  activeColor = 'accent-content',
 }: Props) => {
   return (
-    <div class="flex gap-x-3">
+    <div class='flex gap-x-3'>
       {items?.map((_, index) => {
         const isSelected = index === currentId;
+        const hasCustomColor = activeColor.startsWith('#');
 
         return (
           <button
@@ -35,16 +36,19 @@ const TsCarouselBar = ({
               const target = event.target as HTMLButtonElement;
               onItemClick(Number(target.id));
             }}
-            class={clx("h-1 w-6", !isSelected && "bg-base-100")}
-            style={isSelected
-              ? {
-                backgroundColor: activeColor.startsWith("#")
-                  ? activeColor
-                  : `var(--${activeColor})`,
-              }
-              : {}}
-          >
-          </button>
+            class={clx(
+              'h-1 w-6',
+              !isSelected && 'bg-base-100',
+              isSelected && !hasCustomColor && `bg-${activeColor}`
+            )}
+            style={
+              isSelected && hasCustomColor
+                ? {
+                    backgroundColor: activeColor,
+                  }
+                : {}
+            }
+          ></button>
         );
       })}
     </div>
