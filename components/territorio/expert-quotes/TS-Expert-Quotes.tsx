d31e25c74fe4a418bfcd2c1d4f3d4991dc843a4c
Guilherme <guilherme.brunner@gmail.com>
2024-05-08 10:42:31 -0300

    feat(frontend): adds ExpertQuotes section mobile

--- components/territorio/expert-quotes/TS-Expert-Quotes.tsx
+++ components/territorio/expert-quotes/TS-Expert-Quotes.tsx
@@ -1,5 +1,6 @@
 import Image from "apps/website/components/Image.tsx";
 import TsTypography from "deco-sites/territorio/components/territorio/typography/TS-Typography.tsx";
+import { clx } from "deco-sites/territorio/sdk/clx.ts";
 import TsCarouselBarIsland from "../../../islands/territorio/TS-Carousel.tsx";
 import { BasicImage } from "../types.ts";
 
@@ -25,30 +26,36 @@ function TsExpertQuotes({
   delay = 5000,
 }: TsExpertQuotesProps) {
   return (
-    <div class="flex flex-col my-[7%] ts-section overflow-x-hidden">
+    <div class="flex flex-col my-[7%] ts-section ts-responsive overflow-x-hidden">
       <Image
         src={decorator.src}
         alt={decorator.alt}
         width={69}
         height={69}
-        class="absolute sm:-mt-[2rem] md:-mt-[3rem] sm:-ml-[1.8rem] md:-ml-[2rem] sm:w-[3rem] md:w-[4.3125rem] sm:h-[3rem] md:h-[4.3125rem]"
+        class={clx(
+          "w-[3rem] h-[3rem]", // mobile
+          "md:w-[4.3125rem] md:h-[4.3125rem]", // medium
+        )}
       />
       <TsCarouselBarIsland
-        containerClassName="flex flex-col gap-8 sm:mb-16 md:mb-24"
-        class="flex overflow-x-auto snap-x snap-mandatory scrollbar-hide w-screen"
+        containerClassName="flex flex-col gap-8 mb-20 sm:mb-16 md:mb-24"
+        class="flex overflow-x-auto snap-x snap-mandatory scrollbar-hide w-screen ml-[3rem] md:ml-[4.3125rem]"
         showBar={false}
         autoChangeDelay={delay}
       >
         {Object.entries(quotes).map(([_, quote]) => (
-          <div class="flex flex-col gap-y-8 min-w-full">
+          <div class="flex flex-col sm:gap-y-8 min-w-full">
             <TsTypography
               color="accent-content"
               weight="500"
-              class="text-[4rem] leading-none sm:max-w-[54rem] md:max-w-[61.625rem]"
+              class="text-[2.75rem] sm:text-[4rem] leading-none max-w-[calc(100%-9.4rem)] sm:max-w-[54rem] md:max-w-[61.625rem]"
             >
               {quote.text}
             </TsTypography>
-            <TsTypography color="base-100" class="text-[2.75rem]">
+            <TsTypography
+              color="base-100"
+              class="text-[2rem] sm:text-[2.75rem]"
+            >
               {quote.author}
             </TsTypography>
           </div>
@@ -59,7 +66,7 @@ function TsExpertQuotes({
         alt={mobileImage.alt}
         width={271}
         height={165}
-        class="sm:hidden"
+        class="sm:hidden min-w-[16.9375rem] w-full"
       />
       <Image
         src={desktopImage.src}
