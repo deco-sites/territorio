2af0f26e0f838cd37423e6fcc172c2a1d305f834
Guilherme <guilherme.brunner@gmail.com>
2024-05-16 16:07:51 -0300

    feat(frontend): updates TsCourseHero banner styles

---
+++ islands/territorio/TS-CourseHero.tsx
@@ -0,0 +1,8 @@
+import type { TsCourseHeroProps } from "../../components/territorio/course-hero/TS-Course-Hero-Island.tsx";
+import Component from "../../components/territorio/course-hero/TS-Course-Hero-Island.tsx";
+
+function Island(props: TsCourseHeroProps) {
+  return <Component {...props} />;
+}
+
+export default Island;
