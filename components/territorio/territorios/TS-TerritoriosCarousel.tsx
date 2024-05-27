6b1f319d53c825670e5cbf9f38b2d2f55eeb9f4a
Guilherme <guilherme.brunner@gmail.com>
2024-05-27 16:39:46 -0300

    fix(frontend): fixes on territorios

--- components/territorio/territorios/TS-TerritoriosCarousel.tsx
+++ components/territorio/territorios/TS-TerritoriosCarousel.tsx
@@ -18,9 +18,9 @@ const TsTerritoriosCarousel = ({ items, delay = 5000 }: Props) => {
     >
       {Object.entries(items).map(([_, item]) => (
         <div class="text-center min-w-full flex flex-col gap-1">
-          <div class="flex flex-col items-center gap-y-5">
-            <div class="flex flex-col items-center justify-center max-w-[60%] sm:max-w-[40%]">
-              <div class="flex justify-between items-center gap-x-8 mb-5">
+          <div class="flex flex-col items-center">
+            <div class="flex flex-col items-center justify-center max-w-[60%] sm:max-w-[50%] md:max-w-[40%]">
+              <div class="flex justify-between items-center gap-x-8 mb-[26px] md:mb-4">
                 <Image
                   src={item.image.src}
                   alt={item.image.alt}
@@ -31,14 +31,14 @@ const TsTerritoriosCarousel = ({ items, delay = 5000 }: Props) => {
                 <TsTypography
                   weight="300"
                   color="accent-content"
-                  class="text-5xl text-left max-w-[60%]"
+                  class="text-[24px] leading-[24px] sm:text-5xl text-left max-w-[60%]"
                 >
                   Território{" "}
                   <TsTypography weight="600">{item.title}</TsTypography>
                 </TsTypography>
               </div>
               <div class="text-left">
-                <TsRichText class="text-base-100 w-full font-body text-2xl">
+                <TsRichText class="text-base-100 w-full font-body text-[14px] leading-[21px] sm:text-3xl md:text-2xl">
                   {item.description}
                 </TsRichText>
               </div>
