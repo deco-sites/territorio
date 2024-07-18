9b1f9cdc28c1d45db777461b799bda77be91fb03
MatheusTrindade5201 <matheus.trindade5201@hotmail.com>
2024-07-18 20:49:12 -0300

    chore: rename component

--- islands/territorio/TS-CheckoutButton.tsx
+++
@@ -1,8 +0,0 @@
-import type { TsCheckoutButtonIslandProps } from "../../components/territorio/checkout-button/TS-Checkout-Button-Island.tsx";
-import Component from "../../components/territorio/checkout-button/TS-Checkout-Button-Island.tsx";
-
-function Island(props: TsCheckoutButtonIslandProps) {
-  return <Component {...props} />;
-}
-
-export default Island;
